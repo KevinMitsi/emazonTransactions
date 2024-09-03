03a2bfde2b8c7d4363ac6f10afd886951f0f7d83
KevinMitsi <kevinandresgarciaaguirre@gmail.com>
2024-09-03 14:41:34 -0500

    First commit

---
+++ src/main/java/com/kevin/emazon_transacciones/EmazonTransaccionesApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.emazon_transacciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonTransaccionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonTransaccionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevin/emazon_transacciones/EmazonTransaccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.emazon_transacciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonTransaccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
